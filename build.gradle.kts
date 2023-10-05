430aeb063e57d68c014b5a527b85393abcad9495
Jintin <jintin.lin1018@gmail.com>
2023-10-05 22:20:54 +0800

    Update AGP to 8.1.2 (#12)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.library") version "8.1.2" apply false
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("org.jetbrains.kotlin.jvm") version "1.9.10" apply false
 
